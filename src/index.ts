88deae9f23c44887c51d827fc719c91cdf131101
Utkin Denis <stolen.youth@yandex.ru>
2022-10-03 00:42:07 +0700

    feat: webpack added

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import './scss/index.scss'
+
+function sum(a: number, b: number) {
+    return a + b
+}
+
+console.log(sum(2, 3))
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: "development",
+    entry: './index.ts',
+    output: {
+        filename: isProd ? "bundle.[hash].js" : "bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    devtool: isDev ? 'source-map' : false,
+    devServer: {
+        port: 3000,
+        hot: isDev,
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template: "index.html",
+            minify: {
+                removeComments: isProd,
+                collapseWhitespace: isProd
+            }
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                },
+            ],
+        }),
+        new MiniCssExtractPlugin({
+            filename: isProd ? 'bundle.[hash].css' : 'bundle.css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+}
